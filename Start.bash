9cd69c2612af42f03ad7c8fc0fc9826e07756e4e
Jean-Eric <Jean.mommeja@gmail.com>
2021-11-10 16:53:55 +0100

    Ajout CheckIMG CHECK NETWORK FINI LOADURL EN COURS

---
+++ Start.bash
@@ -0,0 +1,5 @@
+
+ bash util/CheckIMG.bash images/cafe.jpeg images/corse.png images/capybara.jpeg images/poney.jpeg 
+ bash util/CheckNetwork.bash
+
+ 
---
+++ util/CheckIMG.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [[ ! -d images ]]
+then
+    echo "Le dossier Image n'existe pas!"
+    exit 1
+else
+echo "ca marche wola" 
+fi
+
+for FILE in ${@}
+do
+  if [[ ! -f $FILE ]]
+  then
+    echo "le fichier ${FILE} n'exite pas ou a été altérer"
+  fi
+done
