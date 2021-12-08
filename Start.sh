0c6f952757f9fa056fba925af4626bcbf75202d2
Jean-Eric <Jean.mommeja@gmail.com>
2021-12-08 13:07:58 +0100

    push html
    test1JM

--- Start.sh
+++ Start.sh
@@ -1,6 +1,7 @@
 
 if [ "$1" == "--help" ]; then
-  echo "Pour demarer ce programme vous aurez besoin d'une connection internet, et lancer le programme en SU (commande Sudo Start.bash)"
+  echo "Pour demarer ce programme vous aurez besoin d'une connection internet, et lancer le programme en SU (commande Sudo Bash Start.sh)"
+  echo "--build : Permet de deplacer le dossier dans l'autre dossier cible."
   exit 0
 fi
 
@@ -10,8 +11,7 @@ if [ "$1" == "--build" ]; then
   exit 0
 fi
 
-
-	 if [[ `id -u` -ne 0 ]] ; then echo " Vous n'etes pas en Super Admin relancer le script en faisant ''Sudo Start.Bash'' Appuiez sur Entrer pour arreter le programme. " ; read ; exit 1 ; fi
+if [[ `id -u` -ne 0 ]] ; then echo " Vous n'etes pas en Super Admin relancer le script en faisant ''Sudo Bash Start.sh'' Appuiez sur Entrer pour arreter le programme. " ; read ; exit 1 ; fi
 	 
  python3 --version || echo "Installation de python3" apt update > /dev/null 2>&1 && apt install python3.8 > /dev/null 2>&1
 
